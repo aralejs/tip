e1f5a2dd9414004ce3cff10b370522f206f1c47d
afc163 <afc163@gmail.com>
2013-07-11 15:05:32 +0800

    add tests for Atip

--- tests/tip-spec.js
+++ tests/tip-spec.js
@@ -4,7 +4,8 @@ define(function(require) {
 
     var $ = require('$');
     var Tip = require('tip');
-    var tip, trigger;
+    var Atip = require('atip');
+    var tip, atip, trigger;
     var TPL = '<div><div data-role="content"></div></div>';
     var CONTENT = '_content_';
 
@@ -26,6 +27,20 @@ define(function(require) {
             });
             tip.show();
             expect(tip.element.find('[data-role=content]').html()).to.be(CONTENT);
+        });
+
+        it('hover trigger', function(done) {
+            tip = new Tip({
+                trigger: trigger,
+                template: TPL,
+                content: CONTENT
+            });
+            expect(tip.element.is(':visible')).to.be(false);
+            trigger.trigger('mouseover');
+            setTimeout(function() {
+                expect(tip.element.is(':visible')).to.be(true);
+                done();
+            }, 80);
         });
 
         it('direction up', function() {
@@ -129,6 +144,121 @@ define(function(require) {
             tip.show();
             expect(tip.element.offset().left).to.be(trigger.offset().left + trigger.width()/2 - tip.element.width() + 10);
         });
+
+    });
+
+    describe('Atip', function() {
+
+        beforeEach(function() {
+            trigger = $('<div style="width:200px;">trigger</div>').appendTo(document.body);
+        });
+
+        afterEach(function() {
+            atip.element.remove();
+            trigger.remove();
+        });
+
+        it('atip is a Tip ', function() {
+            atip = new Atip({
+                trigger: trigger,
+                inViewport: true,
+                content: CONTENT
+            });
+            atip.show();
+            expect(atip.element.find('[data-role=content]').html()).to.be(CONTENT);
+            expect(Atip.superclass.constructor).to.be(Tip);
+        });
+
+        it('stylesheet load', function() {
+            atip = new Atip({
+                trigger: trigger,
+                content: CONTENT
+            });
+            atip.show();
+            expect(atip.element.css('color')).to.be('rgb(219, 124, 34)');            
+        });
+
+        it('themes', function() {
+            atip = new Atip({
+                trigger: trigger,
+                content: CONTENT
+            });
+            atip.show();
+            expect(atip.get('theme')).to.be('yellow');
+            expect(atip.element.hasClass('ui-poptip-yellow')).to.be(true);
+            atip.set('theme', 'white');
+            expect(atip.element.hasClass('ui-poptip-yellow')).to.be(false);
+            expect(atip.element.hasClass('ui-poptip-white')).to.be(true);
+            atip.set('theme', 'blue');
+            expect(atip.element.hasClass('ui-poptip-yellow')).to.be(false);
+            expect(atip.element.hasClass('ui-poptip-white')).to.be(false);
+            expect(atip.element.hasClass('ui-poptip-blue')).to.be(true);
+        });
+
+        it('arrowPosition', function() {
+            atip = new Atip({
+                trigger: trigger,
+                content: CONTENT
+            });
+            atip.show();
+            expect(atip.get('arrowPosition')).to.be(7);
+            expect(atip.get('direction')).to.be('up');
+            atip.set('arrowPosition', 10);
+            expect(atip.get('direction')).to.be('right');
+            atip.set('arrowPosition', 2);
+            expect(atip.get('direction')).to.be('left');
+            atip.set('arrowPosition', 1);
+            expect(atip.get('direction')).to.be('down');
+            atip.set('arrowPosition', 5);
+            expect(atip.get('direction')).to.be('up');
+        });
+
+        it('inViewport top', function() {
+            trigger.css({
+                position: 'absolute',
+                top: 0,
+                left: 10
+            });
+            atip = new Atip({
+                trigger: trigger,
+                content: CONTENT,
+                arrowPosition: 7,
+                height: 100,
+                inViewport: true
+            });
+            expect(atip.get('arrowPosition')).to.be(7);
+            atip.show();
+            expect(atip.get('arrowPosition')).to.be('11');
+            trigger.css({
+                position: null,
+                top: null,
+                left: null
+            });
+        });
+
+        it('inViewport bottom', function() {
+            trigger.css({
+                position: 'absolute',
+                bottom: 0,
+                left: 10
+            });
+            atip = new Atip({
+                trigger: trigger,
+                content: CONTENT,
+                arrowPosition: 1,
+                height: 100,
+                inViewport: true
+            });
+            expect(atip.get('arrowPosition')).to.be(1);
+            atip.show();
+            expect(atip.get('arrowPosition')).to.be('5');
+            trigger.css({
+                position: null,
+                top: null,
+                left: null
+            });
+        });
+
 
     });
 
